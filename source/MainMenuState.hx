f1f4ee62a84e7ccfb9927a7c5bb0ec556efecb17
Pambi69420 <chachnan789@gmail.com>
2022-07-10 02:57:37 +0700

    update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -205,6 +205,8 @@ class MainMenuState extends MusicBeatState
 					{
 						if (curSelected != spr.ID)
 						{
+						  FlxTween.tween(blackbars2, {y: 500}, 1, {ease: FlxEase.expoInOut});
+				      FlxTween.tween(blackbars, {y: 500}, 1, {ease: FlxEase.expoInOut});
 							FlxTween.tween(spr, {alpha: 0}, 1.3, {
 								ease: FlxEase.quadOut,
 								onComplete: function(twn:FlxTween)
