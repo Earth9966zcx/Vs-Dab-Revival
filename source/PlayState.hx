5136f7541084959c186b20e2d38c72976e1715ea
Pambi69420 <chachnan789@gmail.com>
2022-07-16 08:20:14 +0700

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -699,29 +699,30 @@ class PlayState extends MusicBeatState
 			}
 				case 'stage':
 				  {
-						curStage = 'stage';
-						
-						//imported shits from json
-						gf.visible= false;
-						defaultCamZoom = 0.9;
-						
-						var treesky:FlxSprite = new FlxSprite(-600, -300).loadGraphic(Paths.image('sky', 'weekdab'));
-						treesky.antialiasing = true;
-						treesky.scrollFactor.set(0.6, 0.6);
-						treesky.active = false;
-						add(treesky);
-	
-						var treestars:FlxSprite = new FlxSprite(-600, -300).loadGraphic(Paths.image('stars', 'weekdab'));
-						treestars.antialiasing = true;
-						treestars.scrollFactor.set(0.1, 0.1);
-						treestars.active = false;
-						add(treestars);
-	
-						var treeground:FlxSprite = new FlxSprite(-600, -100).loadGraphic(Paths.image('thebigboi', 'weekdab'));
-						treeground.antialiasing = true;
-						treeground.scrollFactor.set(0.9, 0.9);
-						treeground.active = false;
-						add(treeground);
+					defaultCamZoom = 0.9;
+					curStage = 'stage';
+					var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('stageback'));
+					bg.antialiasing = true;
+					bg.scrollFactor.set(0.9, 0.9);
+					bg.active = false;
+					add(bg);
+
+					var stageFront:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('stagefront'));
+					stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
+					stageFront.updateHitbox();
+					stageFront.antialiasing = true;
+					stageFront.scrollFactor.set(0.9, 0.9);
+					stageFront.active = false;
+					add(stageFront);
+
+					var stageCurtains:FlxSprite = new FlxSprite(-500, -300).loadGraphic(Paths.image('stagecurtains'));
+					stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
+					stageCurtains.updateHitbox();
+					stageCurtains.antialiasing = true;
+					stageCurtains.scrollFactor.set(1.3, 1.3);
+					stageCurtains.active = false;
+
+					add(stageCurtains);
 		  }
 				/*
 				case 'treepog':
